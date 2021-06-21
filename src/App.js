64c9b21c50f0d1f9f83edac01f1d99f8df5d3b4c
Sakshi Das <70979810+SakshiDas@users.noreply.github.com>
2021-06-22 01:56:44 +0530

    added history

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
 import React, { useState } from 'react';
 import Board from './components/Board';
+import History from './components/History';
 import { calculateWinner } from './helpers';
 import './styles/root.scss';
 const App = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
+  const [history, setHistory] = useState([
+    { board: Array(9).fill(null), isXNext: true },
+  ]);
+  const [currMove, setCurrMove] = useState(0);
 
-  const winner = calculateWinner(board);
+  const curr = history[currMove];
+  const winner = calculateWinner(curr.board);
   const message = winner
     ? `Winner is ${winner}`
-    : `Next player is ${isXNext ? 'X' : '0'}`;
+    : `Next player is ${curr.isXNext ? 'X' : '0'}`;
+
   const handleSquareClick = position => {
-    if (board[position] || winner) {
+    if (curr.board[position] || winner) {
       return;
     }
-    setBoard(prev => {
-      return prev.map((square, pos) => {
+    setHistory(prev => {
+      const last = prev[prev.length - 1];
+
+      const newBoard = last.board.map((square, pos) => {
         if (pos === position) {
-          return isXNext ? 'X' : '0';
+          return last.isXNext ? 'X' : '0';
         }
 
         return square;
       });
+      return prev.concat({ board: newBoard, isXNext: !last.isXNext });
     });
-    setIsXNext(prev => !prev);
+
+    setCurrMove(prev => prev + 1);
+  };
+
+  const moveTo = move => {
+    setCurrMove(move);
   };
 
   return (
@@ -32,7 +45,8 @@ const App = () => {
         <h1>Tic Tac Toe</h1>
         <p> A react Game</p>
         <h2>{message}</h2>
-        <Board board={board} handleSquareClick={handleSquareClick} />
+        <Board board={curr.board} handleSquareClick={handleSquareClick} />
+        <History history={history} moveTo={moveTo} currMove={currMove} />
       </div>
     </>
   );
